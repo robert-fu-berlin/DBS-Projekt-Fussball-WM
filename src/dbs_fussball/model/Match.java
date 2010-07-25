f2be1f99a2c6499ae9128634b03b1860b5e588d2
elenaweihe <elena_weihe@gmx.de>
2010-07-25 15:48:21 +0200

    getter and setter for set-attributes added to Match.java

--- src/dbs_fussball/model/Match.java
+++ src/dbs_fussball/model/Match.java
@@ -1,7 +1,12 @@
 package dbs_fussball.model;
 
+import java.util.Collections;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.Set;
+
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Iterators;
 
 import active_record.ActiveRecord;
 
@@ -30,5 +35,77 @@ public class Match extends ActiveRecord {
 	public Date getStatingTime() {
 		return new Date(startingTime);
 	}
+	
+	public boolean addPlayerToLineUpTeamA(Person player) {
+		return startingLineUpTeamA.add(player);
+	}
+
+	public boolean addPlayerToLineUpTeamA(Person player, Person... morePlayers) {
+		return startingLineUpTeamA.add(player) | Collections.addAll(startingLineUpTeamB, morePlayers);
+	}
+
+	public boolean lineUpTeamAContains(Person player) {
+		return startingLineUpTeamA.contains(player);
+	}
+
+	public boolean removePlayerFromLineUpTeamA(Person player) {
+		return startingLineUpTeamA.remove(player);
+	}
+
+	public Iterable<Person> playersOfLineUpTeamA() {
+		return Iterables.unmodifiableIterable(startingLineUpTeamA);
+	}
+
+	public Iterator<Person> playersOfLineUpTeamAIterator() {
+		return Iterators.unmodifiableIterator(startingLineUpTeamA.iterator());
+	}
+	public boolean addPlayerToLineUpTeamB(Person player) {
+		return startingLineUpTeamB.add(player);
+	}
+
+	public boolean addPlayerToLineUpTeamB(Person player, Person... morePlayers) {
+		return startingLineUpTeamB.add(player) | Collections.addAll(startingLineUpTeamA, morePlayers);
+	}
+
+	public boolean lineUpTeamBContains(Person player) {
+		return startingLineUpTeamB.contains(player);
+	}
+
+	public boolean removePlayerFromLineUpTeamB(Person player) {
+		return startingLineUpTeamB.remove(player);
+	}
+
+	public Iterable<Person> playersOfLineUpTeamB() {
+		return Iterables.unmodifiableIterable(startingLineUpTeamB);
+	}
+
+	public Iterator<Person> playersOfLineUpTeamBIterator() {
+		return Iterators.unmodifiableIterator(startingLineUpTeamB.iterator());
+	}
+	public boolean addEvent(Event event) {
+		return events.add(event);
+	}
+
+	public boolean addEvent(Event event, Event... moreEvents) {
+		return events.add(event) | Collections.addAll(events, moreEvents);
+	}
+
+	public boolean containsEvent(Event event) {
+		return events.contains(event);
+	}
+
+	public boolean removeEvent(Event event) {
+		return events.remove(event);
+	}
+
+	public Iterable<Event> events() {
+		return Iterables.unmodifiableIterable(events);
+	}
+
+	public Iterator<Event> eventsIterator() {
+		return Iterators.unmodifiableIterator(events.iterator());
+	}
+	
+
 
 }
